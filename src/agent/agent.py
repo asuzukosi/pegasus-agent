1d9cee8cf7fd39a5c41a5f8343097c2166f4c5d1
asuzukosi <keloasuzu@yahoo.com>
2026-02-07 21:06:13 +0000

    feat: add context manager to the agent

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -2,17 +2,19 @@ from typing import AsyncGenerator
 from src.agent.results import AgentEvent, AgentEventType
 from src.client.llm_client import LLMClient
 from src.client.response import StreamEventType
+from src.context.manager import ContextManager
 
 class Agent:
     def __init__(self) -> None:
         self._client = LLMClient()
+        self._context_manager = ContextManager()
         # keep track of session management
         # TODO: each session should have its own client
 
 
     async def run(self, message: str):
         yield AgentEvent.agent_start(message)
-        # TODO: add user message to context as the agent uses the context to generate the responses
+        self._context_manager.add_user_message(message)
         async for event in self._agentic_loop():
             yield event
             if event.type == AgentEventType.TEXT_COMPLETE:
@@ -20,8 +22,9 @@ class Agent:
         yield AgentEvent.agent_end(final_response)
 
     async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
-        messages = [{"role": "user", "content": "Hello, how are you?"}]
         response_text = ''
+        messages = self._context_manager.get_messages()
+
         async for event in self._client.chat_completion(messages=messages, stream=True):
             if event.type == StreamEventType.TEXT_DELTA:
                 if event.text_delta.content:
@@ -30,6 +33,8 @@ class Agent:
                     yield AgentEvent.text_delta(content)
             elif event.type == StreamEventType.ERROR:
                 yield AgentEvent.agent_error(event.error if event.error else 'Unknown error', {})
+
+        self._context_manager.add_assistant_message(response_text or "")
         if response_text:
             yield AgentEvent.text_complete(response_text)
 
