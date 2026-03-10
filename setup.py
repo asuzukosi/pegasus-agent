604f653c9711ff577ee98058499dd3a0532e8e68
asuzukosi <keloasuzu@yahoo.com>
2026-03-10 22:30:28 +0000

    feat: resolve issues with prompt loading path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pegasus-ai',
-    version='0.1.3',
+    version='0.1.4',
     description='A lightweight terminal agent framework for long-running modular agents',
     url='https://github.com/asuzukosi/pegasus',
     author='Asuzu Kosi',
